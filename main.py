84f50b9bb8f695d627d68689d832830b937e5372
Penultimate-Panacea <rian+github@fantozzi.org>
2020-04-23 11:01:56 -0400

    Basic Window

---
+++ main.py
@@ -0,0 +1,25 @@
+from ctypes import windll
+from PyQt5 import QtGui, QtWidgets, uic
+from sys import exit, argv
+
+myappid = 'fantozzi.multibib.1.0'
+windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
+qtcreator_file  = "mainwindow.ui"
+Ui_MainWindow, QtBaseClass = uic.loadUiType(qtcreator_file)
+
+class MyWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+        Ui_MainWindow.__init__(self)
+        self.setWindowIcon(QtGui.QIcon('icon.png'))
+        self.setWindowTitle('MultiBib')
+        self.setFixedSize(1200, 600)
+        self.setupUi(self)
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(argv)
+    window = MyWindow()
+    window.show()
+    exit(app.exec_())
+
