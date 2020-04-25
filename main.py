b6d57b0966cea8076592fb52a0c1edf5f16dd254
Penultimate-Panacea <rian+github@fantozzi.org>
2020-04-25 11:32:53 -0400

    Revised test.bib to test specific cases

--- main.py
+++ main.py
@@ -18,23 +18,43 @@ class MyWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.setWindowTitle('MultiBib')
         self.setFixedSize(680, 490)
         self.setupUi(self)
-        self.dedupFileButton.clicked.connect(self.import_bibfile('d'))
-    #    self.firstFileButton.clicked.connect(self.import_bibfile('1'))
-    #    self.secondFileButton.clicked.connect(self.import_bibfile('2'))
+        self.dedupFileButton.clicked.connect(self.import_bibfile_d)
+        self.firstFileButton.clicked.connect(self.import_bibfile_f)
+        self.secondFileButton.clicked.connect(self.import_bibfile_s)
         self.dedupDB = None
         self.firstDB = None
         self.secondDB = None
+        return
 
-    def import_bibfile(self):
+    def import_bibfile_d(self):
+        filename = QtWidgets.QFileDialog.getOpenFileName(self, filter="BibTeX (*.bib)")
+        with open(str(filename[0]), encoding='utf8') as bibtex_file:
+            bib_database = bibtexparser.load(bibtex_file)
+        self.dedupDB = bib_database
+        return
+
+    def import_bibfile_f(self):
+        filename = QtWidgets.QFileDialog.getOpenFileName(self)
+        print("filename:")
+        print(filename)
+        print(filename[0])
+        if filename[1] == 'BibTeX (*.bib)':
+            with open(str(filename[0]), encoding='utf8') as bibtex_file:
+                bib_database = bibtexparser.load(bibtex_file)
+            print(str(bib_database))
+        else:
+            with open(filename[0], encoding='utf8') as bibtex_file:
+                print(bibtex_file)
+        print("It Worked")
+        self.firstDB = bib_database
+        print("It Imported")
+        return
+
+    def import_bibfile_s(self):
         filename = QtWidgets.QFileDialog.getOpenFileName(self, filter="BibTeX (*.bib)")
         with open(str(filename), encoding='utf8') as bibtex_file:
             bib_database = bibtexparser.load(bibtex_file)
-        if dictcode == 'd':
-            self.dedupDB = bib_database
-        elif dictcode == '1':
-            self.firstDB = bib_database
-        elif dictcode == '2':
-            self.secondDB = bib_database
+        self.secondDB = bib_database
         return
 
 if __name__ == "__main__":
